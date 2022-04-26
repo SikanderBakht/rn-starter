0887e375d0a9a61b388ec6ac578c11e1d304be62
SikanderBakht <sikander.bin.ismail@gmail.com>
2022-04-26 12:37:15 +0400

    reducers learning done

--- src/screens/ColorSquareScreen.js
+++ src/screens/ColorSquareScreen.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import ColorController from '../components/ColorController'
 
 const COLOR_INCREMENT = 15
 
+const reducer = (state, action) => {
+    switch( action.type) {
+        case 'change_red':
+            return (state.red + action.payload > 255 || state.red + action.payload < 0) ? state : {...state, red: state.red + action.payload}
+        case 'change_green':
+            return (state.green + action.payload > 255 || state.green + action.payload < 0) ? state : {...state, green: state.green + action.payload}
+        case 'change_blue':
+            return (state.blue + action.payload > 255 || state.blue + action.payload < 0) ? state : {...state, blue: state.blue + action.payload}
+        default:
+            return state
+    }
+}
+
 
 const ColorSquareScreen = () => {
-    const [red, setRed] = useState(0)
+    //#region use state
+    /*const [red, setRed] = useState(0)
     const [green, setGreen] = useState(0)
     const [blue, setBlue] = useState(0)
 
@@ -23,7 +37,6 @@ const ColorSquareScreen = () => {
                 return
         }
     }
-
     return (
         <View>
             <Text>
@@ -45,6 +58,37 @@ const ColorSquareScreen = () => {
             <View style={{ height: 100, width: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
         </View>
     )
+    */
+    //#endregion
+
+    //#region use reducer
+    const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0})
+    const {red, green, blue} = state
+
+    return (
+        <View>
+            <Text>
+                Square Screen
+            </Text>
+            <ColorController
+                onIncrease={() => dispatch({type: 'change_red', payload: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({type: 'change_red', payload: -1 * COLOR_INCREMENT})}
+                colorName="red" />
+            <ColorController
+                colorName="green"
+                onIncrease={() => dispatch({type: 'change_green', payload: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({type: 'change_green', payload: -1 * COLOR_INCREMENT})} />
+            <ColorController
+                colorName="blue"
+                onIncrease={() => dispatch({type: 'change_blue', payload: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({type: 'change_blue', payload: -1 * COLOR_INCREMENT})} />
+
+            <View style={{ height: 100, width: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
+        </View>
+    )
+    //#endregion
+
+    
 }
 
 
