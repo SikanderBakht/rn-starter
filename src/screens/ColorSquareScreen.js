e0c6f523038471cd6d37622a636d812a2221ee66
SikanderBakht <sikander.bin.ismail@gmail.com>
2022-04-26 10:32:30 +0400

    color pallete with use state done

--- src/screens/ColorSquareScreen.js
+++ src/screens/ColorSquareScreen.js
@@ -4,32 +4,50 @@ import ColorController from '../components/ColorController'
 
 const COLOR_INCREMENT = 15
 
+
 const ColorSquareScreen = () => {
     const [red, setRed] = useState(0)
     const [green, setGreen] = useState(0)
     const [blue, setBlue] = useState(0)
+
+    const setColor = (color, change) => {
+        switch (color) {
+            case 'red':
+                red + change > 255 || red + change < 0 ? null : setRed(red + change)
+                return
+            case 'blue':
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
+                return
+            case 'green':
+                green + change > 255 || green + change < 0 ? null : setGreen(green + change)
+                return
+        }
+    }
+
     return (
         <View>
             <Text>
                 Square Screen
             </Text>
             <ColorController
-                onIncrease={() => setRed(red + COLOR_INCREMENT)}
-                onDecrease={() => setRed(red - COLOR_INCREMENT)}
+                onIncrease={() => setColor('red', COLOR_INCREMENT)}
+                onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
                 colorName="red" />
             <ColorController
                 colorName="green"
-                onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-                onDecrease={() => setGreen(green - COLOR_INCREMENT)} />
+                onIncrease={() => setColor('green', COLOR_INCREMENT)}
+                onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)} />
             <ColorController
                 colorName="blue"
-                onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-                onDecrease={() => setBlue(blue - COLOR_INCREMENT)} />
+                onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+                onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)} />
 
             <View style={{ height: 100, width: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
         </View>
     )
 }
+
+
 
 const styles = StyleSheet.create({
 
